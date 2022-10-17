b379c6ce18cdcdf992ebe40853cb89b27a636f76
KHY <hyperion0258@gmail.com>
2022-10-17 21:57:03 +0900

    [김희윤] - master : MVC와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,19 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    public String hello(Model model) {
+    public String hello(Model model) { // Model 객체는 컨트롤러에서 데이터를 생성해 View에 전달한다. key와 value값을 저장한다.
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { // @RequestParam은 URL로 파라미터를 전달한다. (쿼리스트링 방식)
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
