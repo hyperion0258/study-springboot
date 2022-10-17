ac1e92e1b321508aade66ba51379fd68cf022cb3
KHY <hyperion0258@gmail.com>
2022-10-17 22:36:59 +0900

    [김희윤] - master : API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,42 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) { // @RequestParam은 URL로 파라미터를 전달한다. (쿼리스트링 방식)
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-String")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api") // http://localhost:8080/hello-api?name=spring&idx=123
+    @ResponseBody // View 페이지가 아닌 자바 객체를 JSON형태로 반환하고 싶을때 @ResponseBody를 사용한다
+    public Hello helloApi(@RequestParam("name") String name, @RequestParam("idx") int idx) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        hello.setIdx(idx);
+        return hello; // {"name":"spring","idx":123}
+    }
+
+    static class Hello {
+        private String name;
+
+        private int idx;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getIdx() {
+            return idx;
+        }
+
+        public void setIdx(int idx) {
+            this.idx = idx;
+        }
     }
 }
