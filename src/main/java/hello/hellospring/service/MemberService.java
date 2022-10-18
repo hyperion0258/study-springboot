823bea0afa9befb32f4420029df8c217316fc977
김희윤 <hyperion0258@gmail.com>
2022-10-18 13:01:49 +0900

    [김희윤] - master : 회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.get(); // Optional에서 값을 꺼내고 싶으면 get()메서드 사용
+//        result.ifPresent(m -> { // Optional로 감쌌기 때문에 ifPresent(값이 있으면)를 쓸수있다
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) // memberRepository.findByName은 반환타입이 Optional이기 때문에 바로 ifPresent를 쓸수있다
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
