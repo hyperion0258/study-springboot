f7f325111ed2823a276b8f5091c5d3b1e8faa1f4
KHY <hyperion0258@gmail.com>
2022-10-22 23:49:57 +0900

    [김희윤] - master : JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // @Entity가 붙으면 jpa가 관리한다는 뜻
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // @Id는 pk이고 @GeneratedValue(strategy = GenerationType.IDENTITY)의 IDENTITY는 db가 알아서 생성해준다
     private Long id; // 임의의 값, 고객이 정하는 id가 아닌 시스템이 정하는 id
     private String name;
 
